1f182bdbeccb8480131481f69f3ae6d3438d8ed5
Iacopo Boccalari <iacopo.boccalari@gmail.com>
2014-01-18 09:55:56 +0100

    test commit
    just a test

---
+++ js/mobiusold.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+		var fileInput = document.getElementById('fileInput');
+		var fileDisplayArea = document.getElementById('displayFile');
+
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var textType = /text.*/;
+
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+
+				reader.onload = function(e) {
+					fileDisplayArea.innerText = reader.result;
+				}
+
+				reader.readAsText(file);	
+			} else {
+				fileDisplayArea.innerText = "File not supported!";
+			}
+		});
+}
