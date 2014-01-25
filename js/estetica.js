c9a4738e2b521708095b7cbc20fd21926de05d4d
Iacopo Boccalari <iacopo.boccalari@gmail.com>
2014-01-25 16:16:14 +0100

    Obbligo di upload file
    L’utente è obbligato a caricare un file e il file deve avere la giusta
    versione del firmware.

---
+++ js/estetica.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+	var length = $('.header').height();
+
+	$(window).scroll(function () {
+
+		var scroll = $(this).scrollTop();
+
+	   if (scroll > length) {
+
+			$('.topBar').css({
+				'position': 'fixed',
+				'top': '0'
+			});
+		}
+
+		else {
+			$('.topBar').css({
+				'position': 'relative',
+				'top': '0'
+			});
+		}
+	});
+});
